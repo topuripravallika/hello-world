ca663afff9e3e14cbfe81d67a314270aacc03d7f
topuripravallika <117137477+topuripravallika@users.noreply.github.com>
2022-11-05 17:31:21 +0530

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,7 @@
+#check two integers they are equl or not
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+if a==b:
+    print(a,b, "are equal")
+else:
+    print(a,b, "are not equal")
